6a0f427324ead87ebf4ea9314513b1b4a06e464a
Nelson13 <1150829@isep.ipp.pt>
2020-10-25 15:24:19 +0000

    Criacao de projeto laravel com jetstream + migrations

---
+++ tasklist/database/migrations/2020_10_25_151858_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->index();
+            $table->string('name');
+            $table->float('perc_done')->default(0);
+            $table->boolean('is_concluded')->default(false);
+            $table->timestamp('final_date')->nullable(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
